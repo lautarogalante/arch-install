bb409b4a25d071c96361d5fcdc36be780b900485
Lautaro Galante <lautaro9galante@gmail.com>
2023-12-05 18:05:18 -0300

    Actualización de los paquetes de mi preferencia

--- post-install-2.sh
+++ post-install-2.sh
@@ -1,22 +1,15 @@
-
-sudo pacman -S xorg-xwayland wlroots qt6-wayland sway waybar polkit-kde-agent sddm pipewire pipewire-pulse wireplumber pavucontrol alacritty mupdf sxiv chromium git
+# Estos son paquetes para mi configuración de escritorio.
+sudo pacman -S sway polkit-kde-agent sddm pulseaudio alsa-utils pavucontrol alacritty mupdf sxiv chromium git
 
 sudo systemctl enable sddm.service
-systemctl --user enable pipewire.service
-systemctl --user enable pipewire-pulse.service
-systemctl --user enable wireplumber.service
 
 mkdir ~/.config/ 
 mkdir ~/.config/sway
-mkdir ~/.config/waybar
 
-
+cd ~/
 mkdir ~/Git
 cd !$ 
+
 git clone https://github.com/lautarogalante/dotfiles.git
-
 sudo cp ~/Git/dotfiles/config/sway/config ~/.config/sway/config
-sudo cp ~/Git/dotfiles/config/waybar/config ~/.config/waybar/config
-sudo cp ~/Git/dotfiles/config/waybar/style.css ~/.config/waybar/style.css
-
 
